10ba3ad9c5a5a814fc16fa57e85a8e88e81fc74a
vimm0 <vimmrana0@gmail.com>
2020-03-16 13:25:01 +0545

    Add example 7 and 8

---
+++ dart/example7.dart
@@ -0,0 +1,9 @@
+void main(){
+  print("this is a string");
+
+  var x = 1.4;
+  var y = 2;
+  // string interpolation
+  var a = "Add numbers $x and $y is ${x + y}";
+  print(a);
+}
---
+++ dart/example8.dart
@@ -0,0 +1,22 @@
+void main() {
+  bool t = true;
+  bool f = false;
+
+  // == equal to
+  // != not equal to
+  // > greater than
+  // < less than
+  // > = greater than or equal to
+  // < = less than or equal to
+  // ! negation
+
+  if(t){
+    print("It was True");
+  }
+
+  if(f) {
+    print("It was False");
+  } else {
+    print("It was false above");
+  }
+}
